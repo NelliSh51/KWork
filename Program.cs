961c1546b1fce7f8f9966184ba6a02b40a583ee6
Nelli <nelli_shamshina@mail.ru>
2022-08-04 20:04:19 +0300

    solution

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿namespace kontrolnaia
+// Написать программу, которая из имеющегося массива строк, формирует массив из строк, длина которых меньше либо равна 3 символа.
+{
+    internal class NewBaseType
+    {
+        private static void Main(string[] args)
+        {
+
