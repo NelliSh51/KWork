c8f128b38b6f1e27df0ad7b8b10124cbd9081457
Nelli <nelli_shamshina@mail.ru>
2022-08-04 20:11:01 +0300

    all

--- Program.cs
+++ Program.cs
@@ -5,4 +5,24 @@
     {
         private static void Main(string[] args)
         {
+            string[] A;
+            Console.WriteLine("Введите массив строк через пробел");
+            string stroka = Console.ReadLine();
+            A = stroka.Split(' ');
+            var result = new string[A.Length];
+            var realSize = 0;
+            foreach (var value in A)
+            {
+                if (value.Length <= 3)
+                {
+                    result[realSize] = value;
+                    realSize++;
+                }
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, result, 0, realSize));
+            Console.ReadKey(true);
+        }
+    }
+}   
+
 
